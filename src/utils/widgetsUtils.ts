1309116fc9afa483436dec8635e24ae5fc43a9f4
iefan <66840141+iefanx@users.noreply.github.com>
2025-02-27 05:45:04 +0530

    fix

--- src/utils/widgetsUtils.ts
+++ src/utils/widgetsUtils.ts
@@ -8,6 +8,26 @@ export type WidgetData = {
 	link: string;
 };
 
+const defaultWidgetsData: WidgetData[] = [
+	{ type: WidgetType.BookmarkWidget, name: "Google", link: "https://www.google.com" },
+	{ type: WidgetType.BookmarkWidget, name: "YouTube", link: "https://www.youtube.com" },
+	{ type: WidgetType.BookmarkWidget, name: "Facebook", link: "https://www.facebook.com" },
+	{ type: WidgetType.BookmarkWidget, name: "Amazon", link: "https://www.amazon.com" },
+	{ type: WidgetType.BookmarkWidget, name: "Wikipedia", link: "https://www.wikipedia.org" },
+	{ type: WidgetType.BookmarkWidget, name: "Twitter", link: "https://www.twitter.com" },
+	{ type: WidgetType.BookmarkWidget, name: "Instagram", link: "https://www.instagram.com" },
+	{ type: WidgetType.BookmarkWidget, name: "LinkedIn", link: "https://www.linkedin.com" },
+	{ type: WidgetType.BookmarkWidget, name: "Netflix", link: "https://www.netflix.com" },
+	{ type: WidgetType.BookmarkWidget, name: "Yahoo", link: "https://www.yahoo.com" },
+];
+
+export const initializeDefaultWidgets = () => {
+	const localStorageWidgetsData = localStorage.getItem("widgetsData");
+	if (!localStorageWidgetsData) {
+		localStorage.setItem("widgetsData", JSON.stringify(defaultWidgetsData));
+	}
+};
+
 export const getWidgets = () => {
 	const localStorageWidgetsData = localStorage.getItem("widgetsData");
 
@@ -60,3 +80,6 @@ export const updateWidgetsData = (newWidgetsData: WidgetData[]) => {
 		console.error("Failed while updating widgets data:", e);
 	}
 };
+
+// Call initializeDefaultWidgets when the module is loaded
+initializeDefaultWidgets();
