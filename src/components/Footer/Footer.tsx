6894eecbe4afb13b734af2ca860abee92d652c9f
iefan <66840141+iefanx@users.noreply.github.com>
2025-02-27 06:05:09 +0530

    fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,12 +1,19 @@
-
 import styles from "./footer.module.scss";
 
 const Footer = () => {
+	const clearLocalStorage = () => {
+		localStorage.clear();
+		alert("Local storage cleared!");
+	};
+
 	return (
 		<footer className={styles.footer}>
-			<p className={styles.description}>
-				This application is a Progressive Web App (PWA) designed to manage all your web applications in one place with a universal search option.
-			</p>
+			{/* <p className={styles.description}>
+				PWA designed to manage all your web apps in one place with a universal search option.
+			</p> */}
+			<button onClick={clearLocalStorage} style={{ color: "#9c988d" , border: "none", padding: "2px", borderRadius: "5px", fontWeight: "bold", cursor: "pointer"}}>
+				Reset
+			</button>
 			
 		</footer>
 	);
